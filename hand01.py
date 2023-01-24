381912ba4f8bf4525e866d34cafd77d4c44f8f83
Havit <havitonline@gmail.com>
2023-01-24 22:29:31 +0530

    hand point detected

---
+++ hand01.py
@@ -0,0 +1,47 @@
+import mediapipe as mp
+import cv2
+import numpy as np
+import uuid
+import os
+
+mp_drawings = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# cam
+cam = cv2.VideoCapture(0)
+
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+
+    while True:
+        ret, frame = cam.read()
+
+        # BGR 2 RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+        # set flag
+        image.flags.writeable = False
+
+        # Detections
+        results = hands.process(image)
+
+        # set flag true
+        image.flags.writeable = True
+
+        # RGB to BGR
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        # rendering Results
+        print(results)
+        
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                mp_drawings.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
+
+
+        cv2.imshow('Track my hand', image)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+cam.release()    
+cv2.destroyAllwindows()
